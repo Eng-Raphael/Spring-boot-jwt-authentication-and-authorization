01cefa0c3d0bafdc6a3b7c83e5d01791deb7c9cb
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2025-09-11 08:40:52 +0300

    adding web mvc config

---
+++ src/main/java/com/raphael/jwttwst/config/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package com.raphael.jwttwst.config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("cors.allowedOrigins")
+    private String allowedOrigins;
+
+    public void addCorsMappings(CorsRegistry registry){
+
+        final Long MAX_AGE_SECS = 3600L;
+
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedHeaders("*")
+                .maxAge(MAX_AGE_SECS);
+
+    }
+
+
+
+}
